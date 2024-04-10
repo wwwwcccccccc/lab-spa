56877db25de79b3529e36c8942e5e0c2ba9a0d1a
wwwwcccccccc <23458356@life.hkbu.edu.hk>
2024-04-10 20:43:39 +0800

    add api

--- api/HttpTrigger1/index.js
+++ api/HttpTrigger1/index.js
@@ -1,40 +1,13 @@
-const fetch = require('node-fetch');
-
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    try {
-        const parsedUrl = new URL(req.url);
-        const url = new URL(`https://sirui-7980.azurewebsites.net${parsedUrl.pathname}${parsedUrl.search}`);
+    const name = (req.query.name || (req.body && req.body.name));
+    const responseMessage = name
+        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
-        const options = {
-            method: req.method,
-            headers: req.headers
-        };
-
-        options.headers.host = url.host;
-
-        if (req.method !== 'GET') {
-            options.body = req.bufferBody;
-        } else if (req.bufferBody) {
-            context.res = {
-                status: 400,
-                body: 'GET requests cannot have a body',
-            };
-            return;
-        }
-
-        const fetchResp = await fetch(url, options);
-        const responseMessage = await fetchResp.text();
-
-        context.res = {
-            status: fetchResp.status,
-            body: responseMessage,
-        };
-    } catch (err) {
-        context.res = {
-            status: 500,
-            body: err.message,
-        };
-    }
-};
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    };
+}
